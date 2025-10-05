7d4e03227ba5c14594c296e1348356beb46fbe96
0xsaltie <elmulayhiyyu@gmail.com>
2025-10-05 22:54:12 +0100

    UPDATE

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
         const removeBtn = document.createElement("button");
         removeBtn.textContent = "Remove";
-        removeBtn.className = "remove-btn";
+        removeBtn.classList.add = "remove-btn";
 
         // Add event to remove the task when button is clicked
         removeBtn.onclick = function() {
